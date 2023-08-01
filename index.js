c341ba079966fddca055a9399a8c590df59ac40e
desmond62 <desmondsolomon623@gmail.com>
2023-08-01 06:08:16 -0700

    added

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 
-document.getElementById('demo').innerHTML = "1. JavaScript: JavaScript is a list of instruction that is ready to be executed when being called or involked. ";
-document.getElementById('demo1').innerHTML = "2.JavaScript Placed in the head.  It is more advisible to place our JavaScript before our body tag because it allows it to load easier and it`s makes it simple for any reader to understand, also it spped up our website.";
+document.getElementById('demo').innerHTML = "1. JavaScript Function: JavaScript Fucntion is a list of instruction that is ready to be executed when being called or involked. ";
+document.getElementById('demo1').innerHTML = "2.It is more advisible to place our JavaScript before our closing body tag, because it allows it to load faster and also makes it  easier for any reader to understand,  it speed up our website.";
 
-document.getElementById('demo2').innerHTML =  "3.Syntax/Variable is a container for storing data.    There are three ways to declare our Variable: Var,Let, Const and declare nothing.....Note:JavaScript have two values, the fixed: which is numbers and strings. Then the Variables: which Var, Let and Const.";
+document.getElementById('demo2').innerHTML =  "3.Variable is a container for storing data.    There are three ways to declare our Variable: Var,Let, Const and declare nothing.....Note:JavaScript have two values, the fixed: which is numbers and strings. Then the Variables: which is Var, Let and Const.";
 document.getElementById('demo3').innerHTML = "4.Var can be redeclare whereas Let and Const cannot do so.";
 document.getElementById('demo4').innerHTML = "5.Output: Output is a way we can display our work in JavaScript, and they are in four ways; Using HTML which is use to display element in our html content. We also have 'window.alert',     'console.log',  'window.write' and 'window.print' and so on.";
-document.getElementById('demo5').innerHTML = "6.JavaScript Comments: Comments are how we comment out our code using // . Note: commenting is good for every programmer, it helps us to save our code for future. remember only u can see it ,the readers can not see it at all";
+document.getElementById('demo5').innerHTML = "6.JavaScript Comments: Comments are how we comment out our code using // . Note: commenting is good for every programmer, it helps us to save our code for future. remember only u can see it ,the readers can not be able to see our comment";
 document.getElementById('demo6').innerHTML = "7.Datatype: Datatype is very important for all the programmers. In datatype we have Primitive and Reference ";
 document.getElementById('demo7').innerHTML = "8.In primitive we have; strings,numbers,null,boolean,sysmbol,and undefined";
-document.getElementById('demo8').innerHTML = "9.Strings deals with alphabet. Typeof strings tells you what you have inside your double quotation and it only accept text, any other thing apart from text will give you undefined...Note:when you have number inside your double quotation the result will give you string not a number";
-document.getElementById('demo9').innerHTML = "10.We have keywords in JavaScript; this,funtion,if,let,const,return,try and for and so on";
-document.getElementById('demo10').innerHTML = "11.We also have JAvaScript Statement; keyword,value,operators,expression and comments";
+document.getElementById('demo8').innerHTML = "9.Strings deals with alphabet. Typeof strings tells you what you have inside your double quotation and it only accept text. Any other thing apart from text will give you an undefined...Note:when you have number inside your double quotation the result will give you string and not a number";
+document.getElementById('demo9').innerHTML = "10.We have keywords in JavaScript; this,funtion,if,let,const,return,try and for etc";
+document.getElementById('demo10').innerHTML = "11.We also have JAvaScript Statement are; keyword,value,operators,expression and comments";
 document.getElementById('demo11').innerHTML = "12.Numbers are also called Operands.....Null is use in storing future information.....Boolean tells us when something is true or false....Undefined tells us when a statement is not been declared properly.";
-document.getElementById('demo12').innerHTML = "13.Reference: we have Object, Array,Function";
+document.getElementById('demo12').innerHTML = "13.In Reference: we have Object, Array,Function";
 document.getElementById('demo15').innerHTML =  "14. OPerators: We have signs like; = assign, + addition, - substraction, * multiplication, ** exponentiation, / division, % modulus, == equal to and so on";
 document.getElementById('demo13').innerHTML = " 15.Function: function is also important in JavaScript.....In function we have function name ,parameter and code which is written inside our curly braces";
 document.getElementById('demo14').innerHTML = "16.Note: our function can only work when we involk or call it....function name can be any simple name apart from keywords....Whenever we have return inside our curly brace we can`t any other code inside it. codes like document.getElementById";
---
+++ work.js
@@ -0,0 +1,18 @@
+// window.onscroll = function() {scrollfunction()};
+
+// function scrollfunction(){
+// if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//     document.getElementById("header").style.fontSize = "30px";
+// } else {
+//     document.getElementById("header").style.fontSize = "90px";
+// }
+// }
+
+// let val= document.getElementById('inp');
+
+// function des(){
+//     val.value+="1";
+// }
+// function das(){
+//     val.value+="2";
+// }
